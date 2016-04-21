3d39588677cee5754602ae5a25d98b1f139d1f64
Dipti Samant <dipti.samant.edu@gmail.com>
2016-04-22 01:48:07 +0530

    Remove extra stemming queries that I have added.

--- src/neu/informationretrieval/project/run1/bm25/BM25Runner.java
+++ src/neu/informationretrieval/project/run1/bm25/BM25Runner.java
@@ -55,25 +55,11 @@ public class BM25Runner {
 			FileReader fileReader;
 			fileReader = new FileReader(fileName);
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
-			int counter = 1;
 			while ((line = bufferedReader.readLine()) != null) {
 				BM25 bm25 = new BM25(outputFolderPath);
 				bm25.rankDocuments(line);
-				counter++;
 			}
 			bufferedReader.close();
-			if( Constants.USE_STEMMING ){ 
-				// Add extra queries from cacm_stem.query.txt if used stemming
-				FileInputStream stream = new FileInputStream("cacm_stem.query.txt");
-				Scanner in = new Scanner(stream);
-				while( in.hasNext() ) {
-					String stemQuery = in.nextLine();
-					BM25 bm25 = new BM25(outputFolderPath);
-					bm25.rankDocuments("" + counter + " " + stemQuery);
-					counter++;
-				}
-				in.close();
-			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
