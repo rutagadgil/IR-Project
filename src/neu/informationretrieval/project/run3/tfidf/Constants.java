d93aa6a0f0226a08798efb3ce93ab6fc904170f9
Ruta Gadgil <gadgil.r@husky.neu.edu>
2016-04-21 20:56:07 -0400

    adding BM25 output

--- src/neu/informationretrieval/project/run3/tfidf/Constants.java
+++ src/neu/informationretrieval/project/run3/tfidf/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
 	public static final String OUTPUT_DIR = "TFIDFOutput";
 	public static final String DOCDIR = "OutputCACMFiles";
 	public static final int TOPDOC_NUMBER = 100;
-	public static final boolean USE_STEMMING = true;
+	public static final boolean USE_STEMMING = false;
 	public static final boolean USE_STOPWORDS = false;
 	public static final String STEMMED_CORPUS_FILENAME = "cacm_stem.txt";
 }
